40a8aca56b2b4fed64f1eedf47d95052392490f1
= <mohamedabobakr04@gmail.com>
2025-09-06 20:48:36 +0200

    feat: used the async handler on the pages controller

--- src/controllers/pagesController.js
+++ src/controllers/pagesController.js
@@ -1,63 +1,43 @@
 import * as pageService from "../services/pagesService.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
 
-export async function createPageHandler(req, res, next) {
-  try {
-    const userId = req.user?.id || 1;
+export const createPageHandler = asyncHandler(async (req, res, next) => {
+  const userId = req.user?.id || 1;
 
-    const data = req.body;
+  const data = req.body;
 
-    if (req.file) {
-      data.backgroundImage = req.file;
-    }
-
-    const page = await pageService.createPage(data, userId);
-
-    res.status(201).json(page);
-  } catch (err) {
-    next(err);
+  if (req.file) {
+    data.backgroundImage = req.file;
   }
-}
 
-export async function getPageHandler(req, res, next) {
-  try {
-    const page = await pageService.getPageById(Number(req.params.id));
-    if (!page) return res.status(404).json({ message: "Page not found" });
-    res.json(page);
-  } catch (err) {
-    next(err);
+  const page = await pageService.createPage(data, userId);
+
+  res.status(201).json(page);
+});
+
+export const getPageHandler = asyncHandler(async (req, res, next) => {
+  const page = await pageService.getPageById(Number(req.params.id));
+  if (!page) return res.status(404).json({ message: "Page not found" });
+  res.json(page);
+});
+
+export const getAllPagesHandler = asyncHandler(async (req, res, next) => {
+  const pages = await pageService.getAllPages();
+  res.json(pages);
+});
+
+export const updatePageHandler = asyncHandler(async (req, res, next) => {
+  const data = req.body;
+  if (req.file) {
+    data.backgroundImage = req.file;
   }
-}
 
-export async function getAllPagesHandler(req, res, next) {
-  try {
-    const pages = await pageService.getAllPages();
-    res.json(pages);
-  } catch (err) {
-    next(err);
-  }
-}
+  const page = await pageService.updatePage(Number(req.params.id), data, 1);
 
-export async function updatePageHandler(req, res, next) {
-  try {
-    // const userId = req.user.id;
-    const data = req.body
-    if (req.file) {
-      data.backgroundImage = req.file;
-    }
+  res.json(page);
+});
 
-    const page = await pageService.updatePage(Number(req.params.id), data, 1);
-
-    res.json(page);
-  } catch (err) {
-    next(err);
-  }
-}
-
-export async function deletePageHandler(req, res, next) {
-  try {
-    await pageService.deletePage(Number(req.params.id));
-    res.status(204).send();
-  } catch (err) {
-    next(err);
-  }
-}
+export const deletePageHandler = asyncHandler(async (req, res, next) => {
+  await pageService.deletePage(Number(req.params.id));
+  res.status(204).send();
+});
