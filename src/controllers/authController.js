e5ad4e0653d652b322ce210b3eea6c3cb0991480
= <mohamedabobakr04@gmail.com>
2025-09-07 08:02:48 +0200

    feat: added the get access token controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,12 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import { loginUser } from "../services/authService.js";
 
+export const getAccessTokenHandler = asyncHandler(async (req, res) => {
+  const { refreshToken } = req.body;
+  const result = await getAccessToken(refreshToken);
+  return res.json(result);
+});
+
 export const loginHandler = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const result = await loginUser(email, password);
